37fd88b9260ddb2bddd8975037bc84d824731169
Klaudia Brzywcy <kbrzywcy@gmail.com>
2022-04-04 16:07:42 +0200

    found and managed bug in list: playersArr  update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ const sortPlayersBtn = document.querySelector('.sort-players');
 const playersContainer = document.querySelector('.players-container');
 const selected1 = document.querySelector('.selected-players1');
 const selected2 = document.querySelector('.selected-players2');
+const notEvenErrDiv = document.querySelector('.not-even-error');
+const notEvenCloseBtn = document.querySelector('.close-btn');
 
 // CREATOR BTNS
 const addPlayerCreatorBtn = document.querySelector('.add-player-creator-btn');
@@ -34,33 +36,37 @@ const ul2 = document.querySelector('.ul2');
 // PLAYER ID
 let playerID = 0;
 
+// AMOUNT OF TEAMS
+const teamAmount = 2;
+
 // CREATING DEFAULT PLAYERS 
-const Perzu = { name: 'Perzu', skill: 2, damage: 2, speed: 4 };
-const Kopec = { name: 'Kopeć', skill: 7, damage: 7, speed: 6 };
-const AntMan = { name: 'Ant-Man', skill: 10, damage: 9, speed: 9 };
-const Dawid = { name: 'Dawid', skill: 5, damage: 4, speed: 6 };
-const Gracjan = { name: 'Gracjan', skill: 8, damage: 6, speed: 10 };
-const Jan = { name: 'Jan', skill: 6, damage: 10, speed: 7 };
-const Biały = { name: 'Biały', skill: 7, damage: 5, speed: 6 };
-const Krzychu = { name: 'Krzychu', skill: 4, damage: 6, speed: 5 };
-const Michu = { name: 'Michu', skill: 2, damage: 1, speed: 8 };
-const Jasion = { name: 'Jasion', skill: 7, damage: 9, speed: 6 };
-const Kajet = { name: 'Kajet', skill: 10, damage: 9, speed: 8 };
-const Tymek = { name: 'Tymek', skill: 10, damage: 9, speed: 10 };
-const Witek = { name: 'Witek', skill: 5, damage: 5, speed: 8 };
-const Darek = { name: 'Darek', skill: 7, damage: 6, speed: 5 };
-const Kebik = { name: 'Kebik', skill: 5, damage: 4, speed: 7 };
-const Kubson = { name: 'Kubson', skill: 8, damage: 7, speed: 8 };
-const Maciek = { name: 'Maciek', skill: 7, damage: 6, speed: 6 };
-const Kacper = { name: 'Kacper', skill: 6, damage: 4, speed: 8 };
-const Niclas = { name: 'Niclas', skill: 6, damage: 7, speed: 5 };
-const Artur = { name: 'Artur', skill: 2, damage: 3, speed: 6 };
+const Perzu = { name: 'Perzu', skill: 2, damage: 2, speed: 4, id: 0 };
+const Kopec = { name: 'Kopeć', skill: 7, damage: 7, speed: 6, id: 1 };
+const AntMan = { name: 'Ant-Man', skill: 10, damage: 9, speed: 9, id: 2 };
+const Dawid = { name: 'Dawid', skill: 5, damage: 4, speed: 6, id: 3 };
+const Gracjan = { name: 'Gracjan', skill: 8, damage: 6, speed: 10, id: 4 };
+const Jan = { name: 'Jan', skill: 6, damage: 10, speed: 7, id: 5 };
+const Biały = { name: 'Biały', skill: 7, damage: 5, speed: 6, id: 6 };
+const Krzychu = { name: 'Krzychu', skill: 4, damage: 6, speed: 5, id: 7 };
+const Michu = { name: 'Michu', skill: 2, damage: 1, speed: 8, id: 8 };
+const Jasion = { name: 'Jasion', skill: 7, damage: 9, speed: 6, id: 9 };
+const Kajet = { name: 'Kajet', skill: 10, damage: 9, speed: 8, id: 10 };
+const Tymek = { name: 'Tymek', skill: 10, damage: 9, speed: 10, id: 11 };
+const Witek = { name: 'Witek', skill: 5, damage: 5, speed: 8, id: 12 };
+const Darek = { name: 'Darek', skill: 7, damage: 6, speed: 5, id: 13 };
+const Kebik = { name: 'Kebik', skill: 5, damage: 4, speed: 7, id: 14 };
+const Kubson = { name: 'Kubson', skill: 8, damage: 7, speed: 8, id: 15 };
+const Maciek = { name: 'Maciek', skill: 7, damage: 6, speed: 6, id: 16 };
+const Kacper = { name: 'Kacper', skill: 6, damage: 4, speed: 8, id: 17 };
+const Niclas = { name: 'Niclas', skill: 6, damage: 7, speed: 5, id: 18 };
+const Artur = { name: 'Artur', skill: 2, damage: 3, speed: 6, id: 19 };
 
 // CREATING PLAYERS ARR
 let playersArr = [Perzu, Kopec, AntMan, Dawid, Gracjan, Jan, Biały, Krzychu, Michu,
     Jasion, Kajet, Tymek, Witek, Darek, Kebik, Kubson, Maciek, Kacper, Niclas, Artur];
 
-let playersCount = playersArr.length
+
+// FUNCTIONS *****************************************************************************
 
 // Creating and adding players
 const openCreator = () => {
@@ -100,19 +106,30 @@ const createNewPlayer = () => {
 
     playerID++;
 
-    let newPlayerObject = { name: `${inputName.value}`, skill: parseInt(inputSkill.value), damage: parseInt(inputDamage.value), speed: parseInt(inputSpeed.value) }
+    let newPlayerObject = { name: `${inputName.value}`, skill: parseInt(inputSkill.value), damage: parseInt(inputDamage.value), speed: parseInt(inputSpeed.value), id: playerID }
     playersArr.push(newPlayerObject)
 }
 
-console.log(playersCount)
-// Removing players
+
+// Removing players from container and playersArr
+let listToRemove = [];
+
 const removeOnePlayer = (id) => {
+
     const playerToRemove = document.getElementById(id);
     playersContainer.removeChild(playerToRemove);
+
+    listToRemove.push(parseInt(playerToRemove.id))
+    console.log(listToRemove)
+    updateArr2();
+
 }
 
+
+// Removing ALL players from container and playersArr
 const removeAllPlayers = () => {
     playersContainer.innerHTML = ``;
+    playersArr = [];
 }
 
 const checkForm = () => {
@@ -150,19 +167,95 @@ const createDefaultPlayers = () => {
 
 playersArr.forEach(el => createDefaultPlayers())
 
+const updateArr1 = () => {
+    for (i = 0; i < playersArr.length; i++) {
+        // console.log(playersArr[i].id)
+
+        if (listToRemove.includes(playersArr[i].id)) {
+            playersArr.splice(i, 1);
+        }
+    }
+}
+
+const updateArr2 = () => {
+    for (i = 0; i < listToRemove.length; i++) {
+        updateArr1();
+        console.log(playersArr);
+    }
+}
 
 
 
 //  **********SORTING PLAYERS TO EACH TEAM ****************
-// let playersCount = playersArr.length
-
 
 const sortPlayers = () => {
-    console.log('sort players')
-    console.log(playersCount)
+    console.log('sort players');
+
+    console.log('final arr')
+    console.log(playersArr)
+
+    // CHECK if teams are even
+    console.log(playersArr.length)
+    if (playersArr.length % 2 !== 0) {
+        openNonEvenErr()
+    } else {
+        sort()
+    }
+}
+
+const sort = () => {
+    console.log('click');
+
+    let teamSize = playersArr.length / 2;
+    let skillSum = 0;
+    let damageSum = 0;
+    let speedSum = 0;
+    for (i = 0; i < playersArr.length; i++) {
+
+        skillSum += playersArr[i].skill;
+
+        damageSum += playersArr[i].damage;
+
+        speedSum += playersArr[i].speed;
+    }
+    // console.log(skillSum);
+    const mediumSkill = Math.round(skillSum / playersArr.length);
+    console.log(mediumSkill);
+
+    // console.log(damageSum);
+    const mediumDamage = Math.round(damageSum / playersArr.length);
+    console.log(mediumDamage);
+
+    // console.log(speedSum);
+    const mediumSpeed = Math.round(speedSum / playersArr.length);
+    console.log(mediumSpeed);
+
+    // Average player total asset
+    const aPTA = Math.round((mediumSpeed + mediumSkill + mediumDamage) / 3);
+    console.log(`aPTA: ${aPTA}`);
+
+    // check aPTA for each player
+    // for (i = 0; i < playersArr.length; i++) {
+
+    // }
+    console.log(playersArr)
+    console.log(teamSize);
+    let team1Arr = [];
+    team1Arr.length = teamSize;
+    let team2Arr = [];
+    team2Arr.length = teamSize;
+
 }
 
 
+const openNonEvenErr = () => {
+    console.log('Nr of players is not even!')
+    notEvenErrDiv.style.display = 'block';
+}
+
+const closeNotEvenErr = () => {
+    notEvenErrDiv.style.display = 'none';
+}
 
 
 // EVENT LISTENERS
@@ -171,3 +264,4 @@ cancelBtn.addEventListener('click', closeCreator);
 addPlayerCreatorBtn.addEventListener('click', addNewPlayer);
 removeAllBtn.addEventListener('click', removeAllPlayers);
 sortPlayersBtn.addEventListener('click', sortPlayers);
+notEvenCloseBtn.addEventListener('click', closeNotEvenErr);
